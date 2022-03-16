e1d8b6b61e43181c09f0e54b0f148e585d389468
sevagenv <sevagenv@gmail.com>
2022-03-16 08:35:33 +0400

    change keys setting for deployement
    Signed-off-by: sevagenv <sevagenv@gmail.com>

--- src/components/API/Supabase.js
+++ src/components/API/Supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import env from "react-dotenv";
 
-const supabaseUrl = env.SUPABASE_URL;
-const supabaseKey = env.SUPABASE_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
